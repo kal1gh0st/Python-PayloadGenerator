62857509c62eb864b2986af7cab6a7fd60ddbccb
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-08-10 10:42:08 +0200

    Update version.py

--- version.py
+++ version.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 #
 # A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2020
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser Public License as published by
