39deb22146f8e29da30c10b360aa3341ebff92fd
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-09-08 12:28:49 +0200

    Delete test_identifier.py

--- WHATpy/tests/test_identifier.py
+++
@@ -1,8 +0,0 @@
-from pywhat import identifier
-
-
-def test_identifier_works():
-    r = identifier.Identifier()
-    out = r.identify("DANHz6EQVoWyZ9rER56DwTXHWUxfkv9k2o")
-    assert "Dogecoin (DOGE) Wallet Address" in out["Regexes"][0]["Regex Pattern"]["Name"]
-
